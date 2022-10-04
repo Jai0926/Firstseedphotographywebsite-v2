6b30995a0ad07dc9b0ec7de34bff51ad23914b0a
Jaivaradhan R <108458259+Jai0926@users.noreply.github.com>
2022-10-04 18:56:09 +0000

    added database

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine,  text
+
+db_connection_string= "mysql+pymysql://wiinpk8tjw52kd7ft470:pscale_pw_3m8gIgP7io4SQHzxv7TlX707xvGfekQnoHrnlc2Zyfi@us-east.connect.psdb.cloud/first_seed09?charset=utf8mb4"
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"}
+    })
+  
+  
+with engine.connect() as conn:
+  result = conn.execute(text("select * from services"))
+  print(result.all())
+
